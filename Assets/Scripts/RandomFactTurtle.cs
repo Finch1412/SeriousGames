b16f7cd5e625fb097c66318c24ce4879d5d64c25
Finch1412 <93916247+Finch1412@users.noreply.github.com>
2025-02-06 14:21:02 +0000

    refereences

--- Assets/Scripts/RandomFactTurtle.cs
+++ Assets/Scripts/RandomFactTurtle.cs
@@ -18,19 +18,19 @@ public class RandomFactTurtle : MonoBehaviour
         switch (factNumber)
         {
             case 0:
-                factBox.text = "Turtles are reptiles.";
+                factBox.text = "All seven species of Sea Turtles are considered threatened or endangered. Two are critically endangered (hawksbill and Kemp’s ridley), one is endangered (green and and three are threatened (leatherback, olive ridley, and loggerhead). Flatbacks are listed as data deficient on the IUCN Red List but are listed as endangered in Australia.";
                 break;
             case 1:
-                factBox.text = "Turtles are cold-blooded.";
+                factBox.text = "It is estimated that only one out of 1,000 hatchlings survives to be an adult. They have many natural predators including birds, crabs, fish, and mammals like racoons. But the female adults can lay thousands of eggs over their lifetimes, so at least a few of them survive to maintain the species.\r\n\r\n";
                 break;
             case 2:
-                factBox.text = "Turtles have a hard shell.";
+                factBox.text = "The sex of sea turtles, like other reptiles, depends on the temperature in the nest. That temperature is generally around 82 degrees F (29 degrees C) though that can vary by species and location";
                 break;
             case 3:
-                factBox.text = "Turtles lay eggs.";
+                factBox.text = "Sea turtles don’t have a favorite food (though most will eat jellyfish.) Each species focuses on different prey for food; the leatherback eats mostly jellyfish, greens primarily eat seagrass, loggerheads prefer crustaceans, and hawksbills eat primarily sea sponges..";
                 break;
             case 4:
-                factBox.text = "Turtles can live in water and on land.";
+                factBox.text = "Some sea turtles migrate very long distances while others stay close to home. Leatherbacks and loggerheads can travel thousands of miles each year, while greens and olive ridleys have shorter migrations, while hawksbills rarely leave a relatively small area.";
                 break;
             default:
                 factBox.text = "Turtles are awesome!";
